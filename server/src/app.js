e878a00476b53644af4c2b75b4056cd29686aa46
Luis Reyes <yuiftw05@gmail.com>
2024-02-20 19:18:05 -0600

    feat: sockeio installed and set up in appjs

--- server/src/app.js
+++ server/src/app.js
@@ -1,4 +1,6 @@
 const express = require('express')
+const socketIO = require('socket.io')
+const http = require('http')
 const cookieParser = require('cookie-parser')
 const bodyParser = require('body-parser')
 const morgan = require('morgan')
@@ -6,13 +8,13 @@ const cors= require('cors')
 const routes = require('./routes/index.js');
 
 
-const server = express()
-server.use(cors())
-server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
-server.use(bodyParser.json({ limit: '50mb' }));
-server.use(cookieParser());
-server.use(morgan('dev'));
-server.use((req, res, next) => {
+const app = express()
+app.use(cors())
+app.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(cookieParser());
+app.use(morgan('dev'));
+app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin',  "*"); // update to match the domain you will make the request from
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
@@ -20,15 +22,33 @@ server.use((req, res, next) => {
   next();
 });
 
-server.use('/speakit', routes);
+app.use('/speakit', routes);
 
 
 // Error catching endware.
-server.use((err, req, res, next) => { // eslint-disable-line no-unused-vars
+app.use((err, req, res, next) => { // eslint-disable-line no-unused-vars
   const status = err.status || 500;
   const message = err.message || err;
   console.error(err);
   res.status(status).send(message);
 });
+
+const server = http.createServer(app)
+
+const io = socketIO(server)
+
+io.on('connection', (socket)=>{
+  console.log("User connected", socket.io)
+
+  socket.on('message',(data)=>{
+    io.emit('message', data)
+  })
+
+  socket.on('disconnect',()=>{
+    console.log('user disconnected', socket.id)
+  })
+  
+})
+
 
 module.exports = server;
