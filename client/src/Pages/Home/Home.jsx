866318bb8c3438ede0844bef1112b06613a5e698
Luis Reyes <yuiftw05@gmail.com>
2024-03-10 19:11:11 -0600

    update: whatever

--- client/src/Pages/Home/Home.jsx
+++ client/src/Pages/Home/Home.jsx
@@ -3,7 +3,11 @@ import mainstyle from "./home.module.css";
 import Navbar from "./components/navbar/navbar";
 import Posts from "./components/posts/posts";
 import Voicechats from "./components/voicechats/voicechats";
+import { UseSelector, useSelector } from "react-redux";
 const Home = () => {
+  const user = useSelector(state=> state.userReducer)
+  console.log(user)
+
   return (
     <>
       <nav className={mainstyle.nvbar}>
