ab12f847d7f0670c102e9bf708fdf8268dd5e2c4
Luis Reyes <yuiftw05@gmail.com>
2024-03-20 10:53:08 -0600

    feat: usercard component working and on click takes user to the user profile with its id. ShowSearch component render error and loading state

--- client/src/Pages/Home/components/ShowSearch/ShowSearch.jsx
+++ client/src/Pages/Home/components/ShowSearch/ShowSearch.jsx
@@ -1,10 +1,15 @@
 
 import styles from './ShowSearch.module.css'
 
+//components
+import UserCard from '../UserCard/UserCard'
+
 const ShowSearch = ({showSearchState, user, isLoading, error})=> {
     return(
         <div className={`${showSearchState ? styles.showSearchMain : styles.notShow}`}>
-
+            {user ? <UserCard user={user}/> : ''}
+            {error ? <p>{error.data}</p> : ''}
+            {isLoading ? <p>loading...</p> : ''}
         </div>
     )
 }
