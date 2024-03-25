814096484fed4c534291cb8d4bcc255461126206
Luis Reyes <yuiftw05@gmail.com>
2024-03-25 08:29:06 -0600

    fix: using ? before accesing to the id

--- client/src/Pages/Profile/Profile.jsx
+++ client/src/Pages/Profile/Profile.jsx
@@ -6,7 +6,7 @@ import styles from './Profile.module.css'
 import { useSelector } from 'react-redux'
 
 import { useParams } from 'react-router-dom';
-import { useGetUserByUsernameQuery } from '../../redux/apiSlices/userAPI'
+import {useGetUserByUsernameQuery} from '../../redux/apiSlices/userAPI'
 
 // components
 import SendMessageButton from './components/SendMessageButton/SendMessageButton';
@@ -15,12 +15,15 @@ const Profile = ()=>{
     // when clicking on someone elses profile or our profile we're extracting the username from the params url
     const {foreignUsername} = useParams()
     console.log(`user visited: ${foreignUsername}`)
+    
     const {data, isLoading, isError, error} = useGetUserByUsernameQuery(foreignUsername)
+    console.log(`user fetch data: ${data}`)
 
     // getting our own username from the gloabl state
     const personalUsername = useSelector(state=> state.userReducer.user)
-    const personalUid = personalUsername.id
-    const foreignId = data.id
+    const personalUid = personalUsername?.id
+    
+    const foreignId = data?.id
     console.log(`user active: ${personalUsername.username}`)
 
     //! comparing that username with the one received from params  if its our own profile, we access to more features
