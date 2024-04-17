1172085a443f8269abb17fe9fd28e879ca7d8631
Luis Reyes <yuiftw05@gmail.com>
2024-04-17 17:51:36 -0600

    feat: adding local storage for the last message sent

--- client/src/Pages/Chats/ChatPreview/ChatPreview.jsx
+++ client/src/Pages/Chats/ChatPreview/ChatPreview.jsx
@@ -1,49 +1,61 @@
-//styles
-import { useSelector } from 'react-redux'
-import styles from './ChatPreview.module.css'
-import { useEffect, useState } from 'react'
+import { useSelector } from 'react-redux';
+import styles from './ChatPreview.module.css';
+import { useEffect, useState } from 'react';
 
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom';
 
-import Skeleton from 'react-loading-skeleton'
+import Skeleton from 'react-loading-skeleton';
 
 //sockets
-import { socket } from '../../../socket'
+import { socket } from '../../../socket';
 
-const ChatPreview = ({lastMessageProp, chatId, userPic, username, name, participantsId})=>{
+const ChatPreview = ({ lastMessageProp, chatId, userPic, username, name, participantsId }) => {
+  const [lastMessage, setLastMessage] = useState("");
 
-    const [lastMessage, setLastMessage] = useState("")
+  useEffect(() => {
+    // Read last message from local storage
+    const savedLastMessage = localStorage.getItem(`lastMessage_${chatId}`);
+    if (savedLastMessage) {
+      setLastMessage(savedLastMessage);
+    }
 
-    useEffect(()=> {
-        socket.on("receiveMessage", (data)=>{
-                if(data.chatId === chatId){
-                    setLastMessage(data.content)
-                }
-          })
+    // Listener for new messages
+    const receiveMessageListener = (data) => {
+      if (data.chatId === chatId) {
+        setLastMessage(data.content);
 
-    },[chatId])
+        // Save last message to local storage
+        localStorage.setItem(`lastMessage_${chatId}`, data.content);
+      }
+    };
 
-    useEffect(() => {
-        socket.connect();
-        socket.emit("joinRoom", chatId);
+    socket.on("receiveMessage", receiveMessageListener);
 
-        return () => {
-            socket.disconnect();
-        }
-    }, [chatId]);
+  }, [chatId]);
 
-    return(
-        
-        <Link to={`/chat/${chatId}`} className={styles.chatPreviewContainer}>
-            <div className={styles.profilePictureContainer}>
-                <img src={userPic} alt={`${name}'s profile picture`} />
-            </div>
-            <div className={styles.nameAndMessageContainer}>
-            <p className={styles.maxTextAndNameContentLimit}>{name || <Skeleton/>} </p>
-            <p className={styles.maxTextAndNameContentLimit}>{lastMessage ? lastMessage : lastMessageProp}</p>
-            </div>
-        </Link>
-    )
-}
+  useEffect(() => {
+    socket.connect();
+    socket.emit("joinRoom", chatId);
 
-export default ChatPreview
+    // at this case, we want to mantain the connection to the socket, so we can receive the messages =>
+    //even tho the component is unmount
+
+    // return () => {
+    //   socket.disconnect();
+    // };
+  }, [chatId]);
+
+  return (
+    <Link to={`/chat/${chatId}`} className={styles.chatPreviewContainer}>
+      <div className={styles.profilePictureContainer}>
+        <img src={userPic} alt={`${name}'s profile picture`} />
+      </div>
+      <div className={styles.nameAndMessageContainer}>
+        <p className={styles.maxTextAndNameContentLimit}>{name || <Skeleton />} </p>
+        <p className={styles.maxTextAndNameContentLimit}>{lastMessage ? lastMessage : lastMessageProp}</p>
+      </div>
+    </Link>
+  );
+};
+
+export default ChatPreview;
