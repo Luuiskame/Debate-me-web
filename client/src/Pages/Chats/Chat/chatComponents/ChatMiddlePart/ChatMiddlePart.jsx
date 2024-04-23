2310d40796a82967d1902b939137f738023ab88e
Luis Reyes <yuiftw05@gmail.com>
2024-04-23 12:41:10 -0600

    styles: adding a different color when the messages are read

--- client/src/Pages/Chats/Chat/chatComponents/ChatMiddlePart/ChatMiddlePart.jsx
+++ client/src/Pages/Chats/Chat/chatComponents/ChatMiddlePart/ChatMiddlePart.jsx
@@ -1,45 +1,58 @@
+import { useState, useEffect } from "react";
+import styles from "./ChatMiddlePart.module.css";
 
-import { useState, useEffect } from 'react'
-import styles from './ChatMiddlePart.module.css'
+import { socket } from "../../../../../socket";
 
-import { socket } from '../../../../../socket'
+import { IoMdCheckmark } from "react-icons/io";
+import { IoCheckmarkDone } from "react-icons/io5";
 
-const ChatMiddlePart = ({messageReceived, correctChatInfo, chatId, personalUserId})=> {
-    const [readMessage, setReadMessage] = useState(null)
-     console.log(messageReceived)
+const ChatMiddlePart = ({
+  messageReceived,
+  correctChatInfo,
+  chatId,
+  personalUserId,
+}) => {
+  const [doubleCheck, setDoubleCheck] = useState(null);
+  console.log(messageReceived);
 
-     useEffect(()=> {
-        if(messageReceived.length > 0 && chatId && personalUserId){
-            const lastMesage = messageReceived.pop()
-            console.log(lastMesage)
-            if(lastMesage?.receiverId === personalUserId && lastMesage.readStatus === false){
-                console.log('activating lastread')
-                socket.emit("updateReadStatus", chatId)
-            }
-        }
-     },[messageReceived])
-    
-    return(
-        <div className={styles.chatMiddlePartContainer}>
+  useEffect(() => {
+    if (messageReceived.length > 0 && chatId && personalUserId) {
+      const lastMesage = messageReceived.pop();
+      console.log(lastMesage);
+      if (
+        lastMesage?.receiverId === personalUserId &&
+        lastMesage.readStatus === false
+      ) {
+        console.log("activating lastread");
+        socket.emit("updateReadStatus", chatId);
+      }
+    }
+  }, [messageReceived]);
 
-            {messageReceived.length > 0 
-            ? messageReceived.map(message=> (
-                <div key={message.id} className={styles.messageCardContainer}>
-                    <figure className={styles.pfpContainer}>
-                        <img src={message.senderPicture} alt="sender picture" />
-                    </figure>
+  return (
+    <div className={styles.chatMiddlePartContainer}>
+      {messageReceived.length > 0 ? (
+        messageReceived.map((message) => (
+          <div key={message.id} className={styles.messageCardContainer}>
+            <figure className={styles.pfpContainer}>
+              <img src={message.senderPicture} alt="sender picture" />
+            </figure>
 
-                    <div className={styles.nameAndMessageContentContainer}>
-                        <p className={styles.name}>{message.senderName}</p>
-                        <p className={styles.message}>{message.content} {message.deliveryStatus ? 'sent' : null} {message.readStatus ? 'visto' : null}</p>
-                        {/* <p>{message.timestamp}</p> */}
-                    </div>
-                </div>
-            )) : (<p>sent any message!</p>)
-            }
+            <div className={styles.nameAndMessageContentContainer}>
+              <p className={styles.name}>{message.senderName}</p>
+              <p className={styles.message}>
+                {message.content}{" "}
+                {message.deliveryStatus ? <IoMdCheckmark className={`${message.readStatus ? `${styles.read}` : null}`}/> : null}{" "}
+              </p>
+              {/* <p>{message.timestamp}</p> */}
+            </div>
+          </div>
+        ))
+      ) : (
+        <p>sent any message!</p>
+      )}
+    </div>
+  );
+};
 
-        </div>
-    )
-}
-
-export default ChatMiddlePart
+export default ChatMiddlePart;
