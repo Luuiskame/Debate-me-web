87dad857f998d9c51409c3b8dfd32ebe70c2d80b
Luis Reyes <yuiftw05@gmail.com>
2024-04-27 12:38:21 -0600

    feat: adding conditional rendering and showing unread messages number when there is more than 0

--- client/src/Pages/Home/components/navbar/components/MessageIcon/MessageIcon.jsx
+++ client/src/Pages/Home/components/navbar/components/MessageIcon/MessageIcon.jsx
@@ -1,59 +1,57 @@
-import styles from './MessageIcon.module.css'
-import { Link } from 'react-router-dom';
+import styles from "./MessageIcon.module.css";
+import { Link } from "react-router-dom";
 
-import { socket } from '../../../../../../socket';
+import { socket } from "../../../../../../socket";
 
 import { FiMessageSquare } from "react-icons/fi";
-import { useEffect, useState } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { setNotReadMessages } from '../../../../../../redux/slices/chatSlice';
+import { useEffect, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { setNotReadMessages } from "../../../../../../redux/slices/chatSlice";
 
-const MessageIcon = ()=> {
-    const [num, setNum] = useState(0)
+const MessageIcon = () => {
+  const [num, setNum] = useState(0);
 
-    useEffect(()=> {
-        console.info('setting num')
-        console.log(num)
-    },[num])
+  useEffect(() => {
+    console.info("setting num");
+    console.log(num);
+  }, [num]);
 
-    const dispatch = useDispatch()
-    const user = useSelector(state=> state.userReducer.user.id)
-    const unreadMessages = useSelector(state=> state.chatsReducer.notReadMessages)
-    console.log(unreadMessages)
+  const dispatch = useDispatch();
+  const user = useSelector((state) => state.userReducer.user.id);
+  const unreadMessages = useSelector(
+    (state) => state.chatsReducer.notReadMessages
+  );
+  console.log(unreadMessages);
 
+  useEffect(() => {
+    socket.on("receiveMessageNotification", (data) => {
+      dispatch(setNotReadMessages(data));
+    });
 
-    useEffect(()=> {
-        socket.on('receiveMessageNotification',data=> {
-            dispatch(setNotReadMessages(data))
-        })
+    //! if we do not stop listening, we are going to set the global state twice
+    return () => {
+      socket.off("receiveMessageNotification");
+    };
+  }, []);
 
-        //! if we do not stop listening, we are going to set the global state twice 
-        return () => {
-            socket.off('receiveMessageNotification');
-        }
-    },[])
+  useEffect(() => {
+    socket.connect();
+    socket.emit("joinRoom", user);
+  }, []);
 
+  return (
+    <>
+      <Link className={styles.link} to="/chats">
+        <FiMessageSquare className={styles.messageIcon} />
+      </Link>
 
-    useEffect(()=> {
-        socket.connect();
-        socket.emit("joinRoom", user)
-    },[])
-    
-    return(
-        <>
-        <Link className={styles.link} to='/chats'>
-            <FiMessageSquare className={styles.messageIcon} />
-            </Link>
-
+      {unreadMessages > 0 ? (
         <div className={styles.messageNotificationNumberContainer}>
-            <p>
-            {unreadMessages}
-            </p>
+          <p>{unreadMessages}</p>
         </div>
+      ) : null}
+    </>
+  );
+};
 
-        </>
-    )
-}
-
-
-export default MessageIcon
+export default MessageIcon;
