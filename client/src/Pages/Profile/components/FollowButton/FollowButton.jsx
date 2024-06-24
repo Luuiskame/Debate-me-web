2cc5bfffa7313a1f4289366d9a96789f1c595b3c
Luis Reyes <yuiftw05@gmail.com>
2024-06-23 22:35:44 -0600

    feat: using redux to make a post and check if we are already following this user

--- client/src/Pages/Profile/components/FollowButton/FollowButton.jsx
+++ client/src/Pages/Profile/components/FollowButton/FollowButton.jsx
@@ -5,37 +5,58 @@ import { socket } from "../../../../socket"
 //react
 import { useEffect, useState } from "react"
 
+// redux
+import { useGetIfFollowingUserMutation } from "../../../../redux/apiSlices/profileAPI"
 
-const FollowButton = ({userToFollow, userWhosFollowing})=> {
-    const [followStatus, setFollowStatus] = useState('Follow')
 
-    const followFunction = ()=> {
-        socket.emit("followUser", {
-            userWhosFollowingId: userWhosFollowing, 
-            userToFollowId: userToFollow
-        })
-
-    }
-
+const FollowButton = ({ userToFollow, userWhosFollowing }) => {
+    const [followStatus, setFollowStatus] = useState('Follow');
+    const [getIfFollowingUser] = useGetIfFollowingUserMutation();
+  
+    const followFunction = () => {
+      socket.emit("followUser", {
+        userWhosFollowingId: userWhosFollowing,
+        userToFollowId: userToFollow
+      });
+    };
+  
     useEffect(() => {
-        const handleFollowUserResponse = (data) => {
-            console.log(data);
-        };
-
-        socket.on("followUserResponse", handleFollowUserResponse);
-
-        // we need to clean the event after component unmount
-        return () => {
-            socket.off("followUserResponse", handleFollowUserResponse);
-        };
-    }, []);
-    
-    return(
-        <button onClick={followFunction}>
-            {followStatus}
-        </button>
-    )
-}
+      
+        const checkFollow = async () => {
+        try {
+          const response = await getIfFollowingUser({
+            userWhosFollowingId: userWhosFollowing,
+            userToFollowId: userToFollow
+          }).unwrap();
+  
+          if (response.isFollowing) {
+            setFollowStatus('Following');
+          }
+        } catch (error) {
+          console.error("Error checking follow status:", error);
+        }
+      };
+  
+      checkFollow();
+  
+      const handleFollowUserResponse = (data) => {
+        console.log(data);
+      };
+  
+      socket.on("followUserResponse", handleFollowUserResponse);
+  
+      // Clean up event listeners after component unmount
+      return () => {
+        socket.off("followUserResponse", handleFollowUserResponse);
+      };
+    }, [userToFollow, userWhosFollowing, getIfFollowingUser]);
+  
+    return (
+      <button onClick={followFunction}>
+        {followStatus}
+      </button>
+    );
+  };
 
 
 export default FollowButton
--- client/src/redux/apiSlices/profileAPI.jsx
+++ client/src/redux/apiSlices/profileAPI.jsx
@@ -1,18 +1,20 @@
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query";
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 const url = "http://localhost:3001/speakit/";
 
-
 export const profileApi = createApi({
-    reducerPath: 'profileApi',
-    baseQuery: fetchBaseQuery({baseUrl: url}),
-    endpoints: (builder)=> ({
-        getIfFollowingUser: builder.mutation({
-            query: (reqbody)=> ({
-                url: "checkFollowStatus",
-                method: "POST",
-                body: ReportBody,
-            })
-        })
-    })
+  reducerPath: 'profileApi',
+  baseQuery: fetchBaseQuery({ baseUrl: url }),
+  endpoints: (builder) => ({
+    getIfFollowingUser: builder.mutation({
+      query: (reqbody) => ({
+        url: "checkFollowStatus",
+        method: "POST",
+        body: reqbody,
+      }),
+    }),
+  }),
+});
 
-})
+export const {
+  useGetIfFollowingUserMutation,
+} = profileApi;
