1b9958f17229a2abdf8ed851fddcce40672a9aa7
Luis Reyes <yuiftw05@gmail.com>
2024-06-30 04:48:50 -0600

    feat: adding notification response from the server when I user is followed to the user that was followed

--- client/src/Pages/Home/components/navbar/components/ProfileIcon/ProfileIcon.jsx
+++ client/src/Pages/Home/components/navbar/components/ProfileIcon/ProfileIcon.jsx
@@ -1,8 +1,22 @@
 import styles from './ProfileIcon.module.css'
 import { CiUser } from "react-icons/ci";
+import { socket } from '../../../../../../socket';
+import { useEffect } from 'react';
 
 
 const ProfileIcon = ()=> {
+
+  useEffect(()=> {
+    const handleNewFollowerNotificationResponse = (data)=> {
+      console.log(data)
+    }
+
+    socket.on("newFollowerNotification", handleNewFollowerNotificationResponse)
+
+    return ()=> {
+      socket.off("newFollowerNotification", handleNewFollowerNotificationResponse)
+    }
+  },[])
 
     return (
        <>
