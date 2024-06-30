db7a604c2f61ee9eff0849979fb77944aefcf9e9
Luis Reyes <yuiftw05@gmail.com>
2024-06-30 05:20:53 -0600

    feat: adding global state to  manage unread followers

--- client/src/Pages/Home/components/navbar/components/ProfileIcon/ProfileIcon.jsx
+++ client/src/Pages/Home/components/navbar/components/ProfileIcon/ProfileIcon.jsx
@@ -2,13 +2,18 @@ import styles from './ProfileIcon.module.css'
 import { CiUser } from "react-icons/ci";
 import { socket } from '../../../../../../socket';
 import { useEffect } from 'react';
-
+import { useDispatch, useSelector } from 'react-redux';
+import { setUnreadFollowers } from '../../../../../../redux/slices/userSlice';
 
 const ProfileIcon = ()=> {
+  const dispatch = useDispatch()
+  const unread = useSelector((state)=> state.userReducer.unreadFollowers)
+  console.log(unread)
 
   useEffect(()=> {
     const handleNewFollowerNotificationResponse = (data)=> {
       console.log(data)
+      dispatch(setUnreadFollowers(data))
     }
 
     socket.on("newFollowerNotification", handleNewFollowerNotificationResponse)
